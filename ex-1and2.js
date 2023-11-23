723b78bc493298206c9099ca8600a388d8f0b281
Gun <cobaltgun2324@gmail.com>
2023-11-23 13:44:38 +0700

    feat: add complete exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const storeName = "Chef's Table by Chef James";
+const netFoodPrice = 15000;
+const totalStoreOpen = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netFoodPrice = 5000;
+totalStoreOpen = 2500;
